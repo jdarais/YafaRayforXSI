ce887dc12a8f6dbbc352629ade9effc5db55027e
povmaniaco <p.alcaide@hotmail.com>
2011-05-10 18:50:34 +0200

    Push all files to page

---
+++ stdafx.cpp
@@ -0,0 +1,28 @@
+/*
+This file is part of YafXSI.
+YafaRay Exporter addon for Autodesk(c) Softimage(c).
+
+Copyright (C) 2010 2011  Pedro Alcaide aka povmaniaco
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+// stdafx.cpp: archivo de código fuente que contiene sólo las inclusiones estándar
+// interface-demo.pch será el encabezado precompilado
+// stdafx.obj contiene la información de tipos precompilada
+
+#include "stdafx.h"
+
+// TODO: mencionar los encabezados adicionales que se necesitan en STDAFX.H
+// pero no en este archivo
