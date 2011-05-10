55b65884f60c5c3a3341b41b2d538ce261c70a17
povmaniaco <p.alcaide@hotmail.com>
2011-05-10 18:50:19 +0200

    Push all files to page

---
+++ stdafx.h
@@ -0,0 +1,37 @@
+/*
+This file is part of YafXSI.
+YafaRay Exporter addon for Autodesk(c) Softimage(c).
+
+Copyright (C) 2010 2011  Pedro Alcaide aka povmaniaco
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+// stdafx.h: archivo de inclusión de los archivos de inclusión estándar del sistema
+// o archivos de inclusión específicos de un proyecto utilizados frecuentemente,
+// pero rara vez modificados
+//
+
+#pragma once
+
+#pragma warning (disable : 4244) 
+#pragma warning (disable : 4996) 
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+// TODO: mencionar aquí los encabezados adicionales que el programa necesita
+#include "yafxsi_main.h"
